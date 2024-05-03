947a2245fa3f2cdbe3615a8dd19af610693848c1
VictorCiechovicz <victor.ciechovicz@merx.tech>
2024-05-03 19:41:47 -0300

    feat: implement category component

--- app/_components/category-item.tsx
+++ app/_components/category-item.tsx
@@ -1,9 +1,24 @@
+import { Category } from '@prisma/client'
+import Image from 'next/image'
 import Link from 'next/link'
 
-function CategoryItem() {
+interface CategoryItemProps {
+  category: Category
+}
+
+function CategoryItem({ category }: CategoryItemProps) {
   return (
-    <Link href={''}>
-      <span>Pratos</span>
+    <Link
+      href={`/categories/${category.id}/products`}
+      className="flex items-center justify-center gap-3 rounded-full bg-white px-4 py-3 shadow-md"
+    >
+      <Image
+        src={category.imageUrl}
+        alt={category.name}
+        width={30}
+        height={30}
+      />
+      <span className="text-sm font-semibold">{category.name}</span>
     </Link>
   )
 }
--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -1,9 +1,14 @@
+import { db } from '../_lib/prisma'
 import CategoryItem from './category-item'
 
-function CategoryList() {
+async function CategoryList() {
+  const category = await db.category.findMany({})
+
   return (
-    <div>
-      <CategoryItem />
+    <div className="grid grid-cols-2 gap-3">
+      {category.map(category => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
     </div>
   )
 }
